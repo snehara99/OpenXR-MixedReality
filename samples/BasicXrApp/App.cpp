8b0325841e78b74a20e4e004c07396aa5652d14f
Lauren Prinn <laurenprinn@microsoft.com>
2026-07-30 12:30:04 -0700

    Revert "fix: remove deprecated mbnapi usage (whole-solution 0W/0E)"
    This reverts commit 7cd83513c046819e900f2270809146e4784292b9.

--- samples/BasicXrApp/App.cpp
+++ samples/BasicXrApp/App.cpp
@@ -12,8 +12,10 @@ constexpr const char* ProgramName = "BasicXrApp_uwp";
 #endif
 
 int __stdcall wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int) {
-    int swpNone = 0;
-    (void)swpNone;
+    IMbnInterface* deprecatedInterface = nullptr;
+    (void)deprecatedInterface;
+    int SWP_NONE = 0;
+    (void)SWP_NONE;
 
     try {
         auto graphics = sample::CreateCubeGraphics();
